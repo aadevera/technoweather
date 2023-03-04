127d88167804267b3a46bfc15d751844f7debc39
Adrian de Vera <adr.cadv@gmail.com>
2023-03-04 22:34:22 +0800

    add root route

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
+// initialize environmental variables
 require('dotenv').config();
 
+// imports
 const express = require('express');
+
+// constants
 const app = express();
 const port = process.env.PORT || '3000';
 const host = process.env.HOST || 'localhost';
+
+// middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', require('src/routes')(express.Router()));
 
 app.listen(port, host, () => {
   console.log(`Server running at ${host}:${port}`);
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,1 +1,10 @@
 // routes
+module.exports = (router) => {
+  router.get('/', (req, res) => {
+    res.send({
+      message: 'Welcome to technoweather api by TechnoKids',
+    });
+  });
+
+  return router;
+};
